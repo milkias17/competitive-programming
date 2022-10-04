723c2efc39f2d00a660bf9a9e39f12d97b09f5c5
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-10-04 13:54:14 +0300

    do smaller numbers challenge

---
+++ SmallerNumbers.java
@@ -0,0 +1,26 @@
+/*
+
+Given the array nums, for each nums[i] find out how many numbers in the
+array are smaller than it. That is, for each nums[i] you have to count
+the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+ **/
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+       int[] result = new int[nums.length];
+       for (int i = 0; i < nums.length; i++) {
+           int count = 0;
+           for (int j = 0; j < nums.length; j++) {
+               if (j == i) {
+                   continue;
+               }
+               if (nums[j] < nums[i]) {
+                   count++;
+               }
+           }
+           result[i] = count;
+       } 
+       return result;
+    }
+}
