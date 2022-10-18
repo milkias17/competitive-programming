e249b396cc795f04629e9d668abb1918b49f968b
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-10-19 01:49:15 +0300

    solve sub array threshold problem

---
+++ num_sumarrays_threshold.py
@@ -0,0 +1,34 @@
+from typing import List
+
+"""
+Given an array of integers arr and two integers k and threshold, return
+the number of sub-arrays of size k and average greater than or equal
+to threshold.
+"""
+
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        count = 0
+        cur_sum = 0
+        window_left = 0
+        window_right = 0
+
+        while window_right < k:
+            cur_sum += arr[window_right]
+            window_right += 1
+
+        if (cur_sum / k) >= threshold:
+            count += 1
+
+        window_right -= 1
+        while window_right < len(arr):
+            window_right += 1
+            if window_right >= len(arr):
+                break
+            cur_sum = cur_sum - arr[window_left] + arr[window_right]
+            window_left += 1
+            if (cur_sum / k) >= threshold:
+                count += 1
+
+        return count
