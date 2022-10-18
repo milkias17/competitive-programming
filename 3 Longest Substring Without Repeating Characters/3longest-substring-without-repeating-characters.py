d997f4f7d8a714be15c3dc31aa421df74621875e
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-10-18 21:00:56 +0000

    Code challenge submission for question: 3 Longest Substring Without Repeating Characters

---
+++ 3 Longest Substring Without Repeating Characters/3longest-substring-without-repeating-characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        occurences = []
+        windowLeft = 0
+        windowRight = 0
+        maxLen = 0
+
+        while windowRight < len(s):
+            if s[windowRight] not in occurences:
+                occurences.append(s[windowRight])
+                windowRight += 1
+            else:
+                maxLen = max(maxLen, windowRight - windowLeft)
+                occurences.pop(0)
+                windowLeft += 1
+
+        maxLen = max(maxLen, len(occurences))
+        return maxLen
