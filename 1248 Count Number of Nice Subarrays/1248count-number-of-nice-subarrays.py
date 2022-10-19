29ab4d504932815075ebe514d4d58520d1b9a3c9
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-10-19 18:49:41 +0000

    Code challenge submission for question: 1248 Count Number of Nice Subarrays

---
+++ 1248 Count Number of Nice Subarrays/1248count-number-of-nice-subarrays.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        res = 0
+        i = 0
+        count = 0
+        for j in range(len(nums)):
+            if nums[j] % 2 != 0:
+                k -= 1
+                count = 0
+
+            while k == 0:
+                k += nums[i] % 2
+                count += 1
+                i += 1
+
+            res += count
+
+        return res
