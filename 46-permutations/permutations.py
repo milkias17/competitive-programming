b45335509147445957c5ade562b0fd3593654835
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-02-05 09:56:03 +0300

    Time: 7 ms (2.31%) | Memory: 19.5 MB (34.21%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,15 +1,23 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        perms = [[]]
-
-        for num in nums:
-            next_perms = []
-            for perm in perms:
-                for i in range(len(perm) + 1):
-                    tmp = perm.copy()
-                    tmp.insert(i, num)
-                    next_perms.append(tmp)
-            
-            perms = next_perms
+    def create_permutations(self, nums: List[int], cur_perms: List[List[int]], perms: List[List[int]]):
+        print(f"Cur: {cur_perms}")
+        if len(nums) == 0:
+            new_copy = []
+            for perm in cur_perms:
+                perms.append(perm.copy())
+            return
         
+        for i, num in enumerate(nums):
+            for perm in cur_perms:
+                perm.append(num)
+                nums_copy = nums.copy()
+                nums_copy.pop(i)
+                self.create_permutations(nums_copy, cur_perms, perms)
+                perm.pop()
+
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms = []
+        self.create_permutations(nums, [[]], perms)
         return perms
+        
