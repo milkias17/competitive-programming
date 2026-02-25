79a8591393efb5f695080d31a5d809ec29347476
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-02-25 19:32:30 +0300

    Time: 79 ms (38.15%) | Memory: 19.5 MB (82.36%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,40 +1,22 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        ref_counter = {}
-        for char in p:
-            ref_counter[char] = ref_counter.get(char, 0) + 1
-        
-        cur_counter = {}
+        ref = Counter(p)
+
         left = 0
-        right = 0
-        out = []
-        while right < len(s):
-            if s[right] not in ref_counter:
-                left = right + 1
-                right = left
-                cur_counter = {}
-                continue
-            
+        cur_counter = {}
+        anagrams = []
+        for right in range(len(s)):
             cur_counter[s[right]] = cur_counter.get(s[right], 0) + 1
-            
             if right - left + 1 == len(p):
-                valid = True
-                for char, count in ref_counter.items():
-                    if cur_counter.get(char, 0) != count:
-                        valid = False
+                for k, v in ref.items():
+                    if k not in cur_counter:
                         break
-                
-                if valid:
-                    out.append(left)
+                    if v != cur_counter[k]:
+                        break
+                else:
+                    anagrams.append(left)
                 
                 cur_counter[s[left]] -= 1
-                if cur_counter[s[left]] == 0:
-                    del cur_counter[s[left]]
                 left += 1
-                right += 1
-            else:
-                right += 1
         
-        return out
-
-
+        return anagrams
