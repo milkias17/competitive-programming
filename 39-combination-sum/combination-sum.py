e937a1816d76d9d46c40508211374609ab3125dc
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-02-26 18:49:50 +0300

    Time: 15 ms (24.52%) | Memory: 19.5 MB (77.90%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,23 +1,18 @@
 class Solution:
-    def backtrack(self, candidates, target, i, cur_path, res):
-        if target == 0:
-            res.append(cur_path.copy())
+    def backtrack(self, candidates, target, i, cur_set, powerset):
+        if sum(cur_set) == target:
+            powerset.append(cur_set.copy())
             return
 
-        if target < 0:
+        if sum(cur_set) > target or i >= len(candidates):
             return
-
-        if i >= len(candidates):
-            return
-
 
         for j in range(i, len(candidates)):
-            cur_path.append(candidates[j])
-            self.backtrack(candidates, target - candidates[j], j, cur_path, res)        
-            cur_path.pop()
-        
+            cur_set.append(candidates[j])
+            self.backtrack(candidates, target, j, cur_set, powerset)
+            cur_set.pop()
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        self.backtrack(candidates, target, 0, [], res)
-        return res
-        
+        powerset = []
+        self.backtrack(candidates, target, 0, [], powerset)
+        return powerset
