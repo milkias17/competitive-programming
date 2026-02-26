34d1a39c628d1e326b3aed40149ff2df0a93d321
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-02-26 19:08:58 +0300

    Time: 27 ms (30.04%) | Memory: 19.2 MB (83.96%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,25 +1,22 @@
 class Solution:
-    def backtrack(self, candidates, target, i):
-        if self.cur_sum == target:
-            self.power_combs.append(self.combs.copy())
+    def backtrack(self, candidates, target, i, cur_set, power_set):
+        if target == 0:
+            power_set.append(cur_set.copy())
             return
-        if self.cur_sum > target:
+        
+        if target < 0:
             return
         
         j = i
         while j < len(candidates):
-            self.combs.append(candidates[j])
-            self.cur_sum += candidates[j]
-            self.backtrack(candidates, target, j + 1)
-            self.combs.pop()
-            self.cur_sum -= candidates[j]
+            cur_set.append(candidates[j])
+            self.backtrack(candidates, target - candidates[j], j + 1, cur_set, power_set)
+            cur_set.pop()
             j += 1
             while j < len(candidates) and candidates[j] == candidates[j - 1]:
                 j += 1
-        
+
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.cur_sum = 0
-        self.power_combs, self.combs = [], []
-        self.backtrack(sorted(candidates), target, 0)
-        return self.power_combs
-        
+        power_set = []
+        self.backtrack(sorted(candidates), target, 0, [], power_set)
+        return power_set
