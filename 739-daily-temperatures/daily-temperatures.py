705dd275db609b86b95f5e14068541d41d59a09b
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-03-10 12:26:31 +0300

    Time: 92 ms (66.35%) | Memory: 28.4 MB (70.77%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,20 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
+        queue = deque()
         answer = [0] * len(temperatures)
-        for i in range(len(temperatures) - 1, -1, -1):
-            if len(stack) == 0:
-                stack.append(i)
-                continue
+
+        for i, temperature in enumerate(temperatures):
+            while queue and temperatures[queue[-1]] < temperature:
+                idx = queue.pop()
+                answer[idx] = i - idx
             
-            cur = temperatures[i]
-            while len(stack) > 0 and temperatures[stack[-1]] <= cur:
-                stack.pop()
-            if len(stack) == 0:
-                stack.append(i)
-            else:
-                next_greater = stack[-1]
-                answer[i] = next_greater - i
-                stack.append(i)
+            queue.append(i)
         
         return answer
