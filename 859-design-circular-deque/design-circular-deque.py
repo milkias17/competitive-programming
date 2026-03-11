ff62b9d8b8ed2a93ac6d5131ac5f489be1494870
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-03-11 18:42:27 +0300

    Time: 3 ms (90.83%) | Memory: 20.7 MB (7.83%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.py
@@ -0,0 +1,95 @@
+class Node:
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.head = None
+        self.limit = k
+        self.tail = None
+        self.size = 0
+        
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        
+        node = Node(value)
+        if self.isEmpty():
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        
+        self.size += 1
+        return True
+        
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        
+        node = Node(value)
+        if self.isEmpty():
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        
+        self.head = self.head.next
+        self.size -= 1
+        return True
+        
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        
+        self.tail = self.tail.prev
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        
+        return self.head.val
+
+    def getRear(self) -> int:
+        if self.size == 0:
+            return -1
+        
+        return self.tail.val
+        
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+        
+    def isFull(self) -> bool:
+        return self.size == self.limit
+        
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
