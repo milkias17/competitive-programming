3ea66914beff12cd3689a50dc215890bc30c4617
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-03-13 13:43:00 +0300

    Time: 123 ms (34.71%) | Memory: 53.6 MB (28.66%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,13 +1,18 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        queue = deque(sorted(points, key=lambda x: (x[1], x[0])))
+        points.sort(key=lambda x: (x[1], x[0]))
         count = 0
+        i = 0
 
-        print(queue)
-        while queue:
-            taken = queue[0][1]
+        # print(points)
+        while i < len(points):
+            point = points[i]
+            hit = point[1]
+
+            while i < len(points) and hit >= points[i][0] and hit <= points[i][1]:
+                i += 1
+            
             count += 1
-            while queue and taken >= queue[0][0] :
-                queue.popleft()
-        
         return count
+
+
