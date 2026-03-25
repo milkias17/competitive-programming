aa98a58870496454e838061266cbf8989138d464
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-03-25 13:48:04 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (29.94%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,25 @@
 class Solution:
-    def backtrack(self, digits: str, mapper: Dict[int, List[str]], i: int, cur: List[str], result: List[str]):
-        if i > len(digits):
+    def backtrack(self, digits, i, cur_set, power_set):
+        if i >= len(digits):
+            power_set.add("".join(cur_set))
             return
         
-        for j in mapper[digits[i]]:
-            cur.append(j)
-            if len(cur) == len(digits):
-                result.append("".join(cur))
-                cur.pop()
-                continue
-
-            self.backtrack(digits, mapper, i + 1, cur, result)
-            cur.pop()
+        for v in self.reps[digits[i]]:
+            cur_set.append(v)
+            self.backtrack(digits, i + 1, cur_set, power_set)
+            cur_set.pop()
 
     def letterCombinations(self, digits: str) -> List[str]:
-        mapper = {
-            '2': ["a", "b", "c"],
-            '3': ["d", "e", "f"],
-            '4': ["g", "h", "i"],
-            '5': ["j", "k", "l"],
-            '6': ["m", "n", "o"],
-            '7': ["p", "q", "r", "s"],
-            '8': ["t", "u", "v"],
-            '9': ["w", "x", "y", "z"],
+        self.reps = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
         }
-        result = []
-        self.backtrack(digits, mapper, 0, [], result)
-        return result
+        power_set = set()
+        self.backtrack(digits, 0, [], power_set)
+        return list(power_set)
