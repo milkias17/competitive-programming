e63d74e17c74d60289efb141d11a1b5de7f20d40
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-02 16:16:07 +0300

    Time: 458 ms (5.73%) | Memory: 24 MB (46.00%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,24 +1,25 @@
 class Solution:
-    def shipWithinDays(self, weights: List[int], days: int) -> int:
-        low = max(weights)
-        high = sum(weights)
-        min_weight = float("inf")
-        while low <= high:
-            mid = low + (high - low) // 2
-            cur_days = 0
-            cur_sum = 0
-            for package in weights:
-                if cur_sum + package > mid:
-                    cur_days += 1
-                    cur_sum =0
-                cur_sum += package
-            cur_days += 1
-            
-            if cur_days <= days:
-                min_weight = min(min_weight, mid)
-                high = mid - 1
-            else:
-                low = mid + 1
+    def isGood(self, weights, days, ship):
+        i = 0
+        while days > 0 and i < len(weights):
+            cur = 0
+            while i < len(weights) and cur + weights[i] <= ship:
+                cur += weights[i]
+                i += 1
+            days -= 1
         
-        return min_weight
+        return i >= len(weights)
 
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        left = 0
+        right = sum(weights) + 1
+
+        while left + 1 < right:
+            mid = (left + right) // 2
+
+            if self.isGood(weights, days, mid):
+                right = mid
+            else:
+                left = mid
+        
+        return right
