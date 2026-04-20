0e03a794d182eb504c404fae54e82dc0fbc501e6
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-20 16:39:13 +0300

    Time: 4 ms (4.47%) | Memory: 19.3 MB (30.22%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,25 +3,39 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def quick_sort(start, end):
-            if start >= end:
-                return
-                
-            pivot = nums[end]
-            left, cur = start, start
-    
-            while cur < end:
-                if nums[cur] <= pivot:
-                    nums[cur], nums[left] = nums[left], nums[cur]
-                    left += 1
-                
-                cur += 1
-            
-            nums[left], nums[end] = nums[end], nums[left]
-            quick_sort(start, left - 1)
-            quick_sort(left + 1, end)
-        
-        quick_sort(0, len(nums) - 1)
+        counter = Counter(nums)
+        red = 0
+        white = counter[0]
+        blue = white + counter[1]
+        t_white = white
+        t_blue = blue
 
-        
-        
+        ptr = 0
+        while ptr < len(nums) and (red < t_white or white < t_blue or blue < len(nums)):
+            cur = nums[ptr]
+            should = None
+            if cur == 0:
+                if red == ptr:
+                    red += 1
+                    ptr += 1
+                else:
+                    nums[red], nums[ptr] = nums[ptr], nums[red]
+                    while red < len(nums) and nums[red] == 0:
+                        red += 1
+            elif cur == 1:
+                if white == ptr:
+                    white += 1
+                    ptr += 1
+                else:
+                    nums[white], nums[ptr] = nums[ptr], nums[white]
+                    while white < len(nums) and nums[white] == 1:
+                        white += 1
+            else:
+                if blue == ptr:
+                    blue += 1
+                    ptr += 1
+                else:
+                    nums[blue], nums[ptr] = nums[ptr], nums[blue]
+                    while blue < len(nums) and nums[blue] == 2:
+                        blue += 1
+            
