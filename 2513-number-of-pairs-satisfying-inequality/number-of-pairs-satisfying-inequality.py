d03c33f7843c946e48797d5d32d80aeaf7a5747e
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-20 11:45:29 +0300

    Time: 315 ms (77.19%) | Memory: 37.7 MB (69.30%) - LeetSync

--- 2513-number-of-pairs-satisfying-inequality/number-of-pairs-satisfying-inequality.py
+++ 2513-number-of-pairs-satisfying-inequality/number-of-pairs-satisfying-inequality.py
@@ -1,23 +1,14 @@
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
         arr = [n1 - n2 for n1, n2 in zip(nums1, nums2)]
-        self.count = 0
 
-        def merge_sort(arr):
-            if len(arr) <= 1:
-                return arr
-            
-            mid = len(arr) // 2
-            left = merge_sort(arr[:mid])
-            right = merge_sort(arr[mid:])
+        seen = []
+        res = 0
 
-            i = 0
-            for j in range(len(right)):
-                while i < len(left) and left[i] <= right[j] + diff:
-                    i += 1
-                self.count += i
-            
-            return sorted(left + right)
+        for num in arr:
+            count = bisect_right(seen, num + diff)
+            res += count
+
+            insort(seen, num)
         
-        merge_sort(arr)
-        return self.count
+        return res
