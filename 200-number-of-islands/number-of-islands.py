875675f5e9926a2e1e1519dd5f3c70748a4f220a
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-20 19:25:07 +0300

    Time: 300 ms (6.08%) | Memory: 31.4 MB (5.51%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         visited = set()
+        rows, cols = len(grid), len(grid[0])
 
-        def dfs(i, j):
-            if (
-                (i, j) in visited
-                or min(i, j) < 0
-                or i >= len(grid)
-                or j >= len(grid[0])
-                or grid[i][j] == "0"
-            ):
+        def dfs(row, col):
+            if min(row, col) < 0 or row >= rows or col >= cols or (row, col) in visited or grid[row][col] == "0":
                 return
+            
+            visited.add((row, col))
+            directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
 
-            visited.add((i, j))
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-            dfs(i + 1, j)
-            dfs(i - 1, j)
+            for dx, dy in directions:
+                n_row, n_col = row + dx, col + dy
+                dfs(n_row, n_col)
+        
 
         count = 0
-        for i, row in enumerate(grid):
-            for j, val in enumerate(row):
-                if val == "0" or (i, j) in visited:
+        for i in range(rows):
+            for j in range(cols):
+                if (i, j) in visited or grid[i][j] == "0":
                     continue
                 count += 1
                 dfs(i, j)
-
+        
         return count
+            
