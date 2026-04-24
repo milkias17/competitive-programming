6a38e624345f3f6b43b67c3556a165fde1b9bb2f
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-24 17:08:22 +0300

    Time: 4 ms (34.43%) | Memory: 19.6 MB (11.48%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
@@ -11,37 +11,31 @@ class Solution:
         for start, end in blueEdges:
             blue_adj[start].append(end)
 
-        def bfs(start, end):
-            queue = deque()
-            queue.append((start, "r", 0))
-            queue.append((start, "b", 0))
-            visited = set([(start, "r"), (start, "b")])
+        res = [-1] * n
+        queue = deque()
+        queue.append((0, "r", 0))
+        queue.append((0, "b", 0))
+        visited = set([(0, "r"), (0, "b")])
 
-            while queue:
-                cur, color, length = queue.popleft()
-                if cur == end:
-                    return length
+        while queue:
+            cur, color, length = queue.popleft()
+            if res[cur] == -1:
+                res[cur] = length
 
-                if color == "r":
-                    for neigh in blue_adj[cur]:
-                        state = (neigh, "b", length + 1)
-                        if state[:2] in visited:
-                            continue
+            if color == "r":
+                for neigh in blue_adj[cur]:
+                    state = (neigh, "b", length + 1)
+                    if state[:2] in visited:
+                        continue
 
-                        queue.append(state)
-                        visited.add(state[:2])
-                else:
-                    for neigh in red_adj[cur]:
-                        state = (neigh, "r", length + 1)
-                        if state[:2] in visited:
-                            continue
-                        queue.append(state)
-                        visited.add(state[:2])
-
-            return -1
-
-        res = []
-        for i in range(n):
-            res.append(bfs(0, i))
+                    queue.append(state)
+                    visited.add(state[:2])
+            else:
+                for neigh in red_adj[cur]:
+                    state = (neigh, "r", length + 1)
+                    if state[:2] in visited:
+                        continue
+                    queue.append(state)
+                    visited.add(state[:2])
 
         return res
