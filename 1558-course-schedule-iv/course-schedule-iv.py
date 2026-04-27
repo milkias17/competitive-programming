b1d180d66fdc2e25e901a2c690d69f9fdedb8338
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-27 18:55:24 +0300

    Time: 41 ms (58.37%) | Memory: 22.5 MB (48.42%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -2,32 +2,26 @@ class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         adj = {i: [] for i in range(numCourses)}
         reachables = {i: set() for i in range(numCourses)}
+        in_degree = [0 for i in range(numCourses)]
 
         for prereq, course in prerequisites:
             adj[prereq].append(course)
+            in_degree[course] += 1
+        
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
 
-        visited = set()
-        def dfs(course):
-            visited.add(course)
-            for neigh in adj[course]:
-                if neigh in visited:
-                    tmp = reachables[neigh].union(set([neigh]))
-                    reachables[course].update(tmp)
-                else:
-                    tmp = dfs(neigh)
-                    reachables[course].update(tmp)
-            
-            return reachables[course].union(set([course]))
-                    
-        for course in range(numCourses):
-            if course not in visited:
-                dfs(course)
+        while queue:
+            cur = queue.popleft()
+
+            for neigh in adj[cur]:
+                reachables[neigh].update(reachables[cur], set([cur]))
+                in_degree[neigh] -= 1
+                if in_degree[neigh] == 0:
+                    queue.append(neigh)
         
         res = []
         for prereq, course in queries:
-            if course in reachables[prereq]:
-                res.append(True)
-            else:
-                res.append(False)
+            res.append(prereq in reachables[course])
         
         return res
+
