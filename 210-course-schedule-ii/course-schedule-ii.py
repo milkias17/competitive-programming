c0fdbefa59721261eaf781e500591a27d9390155
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-28 16:49:41 +0300

    Time: 0 ms (100.00%) | Memory: 21.2 MB (26.84%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,25 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = {i: [] for i in range(numCourses)}
-        in_order = {i: 0 for i in range(numCourses)}
-        for course, prerequisite in prerequisites:
-            adj[prerequisite].append(course)
-            in_order[course] += 1
+        adj = defaultdict(list)
+        for course, prereq in prerequisites:
+            adj[prereq].append(course)
         
-        queue = deque()
-        for k, v in in_order.items():
-            if v == 0:
-                queue.append(k)
-        
-        res = []
-        while queue:
-            prerequisite = queue.popleft()
-            for course in adj[prerequisite]:
-                in_order[course] -= 1
-                if in_order[course] <= 0:
-                    queue.append(course)
-            
-            res.append(prerequisite)
-        
-        return res if len(res) == numCourses else []
+        stack = []
+        colors = [0 for i in range(numCourses)]
 
+        def dfs(cur):
+            colors[cur] = 1
+
+            for neigh in adj[cur]:
+                if colors[neigh] == 1:
+                    return False
+                if colors[neigh] != 2 and not dfs(neigh):
+                    return False
+            
+
+            stack.append(cur)
+            colors[cur] = 2
+            return True
+        
+        for i in range(numCourses):
+            if colors[i] == 0 and not dfs(i):
+                return []
+        
+        return stack[::-1]
+                
