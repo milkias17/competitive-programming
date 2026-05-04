b1d57c6722141ed338fa9a804e40ffa01d7302c3
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-05-04 16:53:37 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (75.13%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        table = [[-1 for _ in range(n)] for _ in range(m)]
+        for i in range(n):
+            table[-1][i] = 1
         
-        def dfs(row, col, memo):
-            if row == m - 1 and col == n - 1:
-                return 1
-            
-            if row >= m or col >= n:
-                return 0
-            
-            if (row, col) in memo:
-                return memo[(row, col)]
-            
-            memo[(row, col)] = dfs(row + 1, col, memo) + dfs(row, col + 1, memo)
-            return memo[(row, col)]
+        for i in range(m):
+            table[i][-1] = 1
         
-        return dfs(0, 0, {})
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
+                table[i][j] = table[i][j + 1] + table[i + 1][j]
+
+        return table[0][0]
