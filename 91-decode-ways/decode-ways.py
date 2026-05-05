41a7d13591691df55eb60c659fbce08e62ad9f15
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-05-05 12:07:38 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.97%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,24 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        def dfs(i, memo={}):
-            if i >= len(s):
-                return 1
-            
-            if i in memo:
-                return memo[i]
-            
-            count = 0
+        dp = [0] * (len(s) + 1)
+        dp[-1] = 1
+        for i in range(len(s) - 1, -1, -1):
             if s[i] != "0":
-                count += dfs(i + 1, memo)
+                dp[i] += dp[i + 1]
             
-            if i + 1 < len(s):
-                if s[i] == "1":
-                    count += dfs(i + 2, memo)
-                elif s[i] == "2" and s[i + 1] <= "6":
-                    count += dfs(i + 2, memo)
+            if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] <= "6"):
+                dp[i] += dp[i + 2]
 
-            memo[i] = count
-            return count
-
-        
-        return dfs(0)
+        return dp[0]
