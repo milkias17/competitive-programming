17a5bf6eb52a257c7893b0a6894d663be010970d
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-05-11 15:47:41 +0300

    Time: 1916 ms (18.41%) | Memory: 35.8 MB (24.61%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,21 +4,45 @@ class Solution:
         if s % 2 != 0:
             return False
 
-        memo = {}
+        target = s // 2
 
-        def dp(i, target):
-            if target == 0:
-                return True
+        memo = [[None for t in range(target + 1)] for _ in range(len(nums) + 1)]
+        for i in range(len(nums) + 1):
+            memo[i][0] = True
             
-            if target < 0 or i >= len(nums):
-                return False
-            
-            if (i, target) in memo:
-                return memo[(i, target)]
+        for t in range(1, target + 1):
+            memo[-1][t] = False
+        
+        for i in range(len(nums) - 1, -1, -1):
+            cur = nums[i]
+            for t in range(target + 1):
+                res = memo[i + 1][t]
+                if t - cur >= 0:
+                    res = res or memo[i + 1][t - cur]
+                
+                memo[i][t] = res
+        
+        return memo[0][target]
+                
 
-            
-            res = dp(i + 1, target - nums[i]) or dp(i + 1, target)
-            memo[(i, target)] = res
-            return res
+        # def dfs(i, target):
+        #     if target == 0:
+        #         return True
+        #     if target < 0:
+        #         return False
 
-        return dp(0, s // 2)
+        #     if i >= len(nums):
+        #         return False
+            
+        #     if memo[i][target] is not None:
+        #         return memo[i][target]
+            
+            
+        #     res =  dfs(i + 1, target - nums[i]) or dfs(i + 1, target)
+        #     memo[i][target] = res
+        #     return res
+        
+        # return dfs(0, target)
+
+        # [1, 5, 11, 5] -> True(11)
+        # [1, 2, 3, 5] -> False
