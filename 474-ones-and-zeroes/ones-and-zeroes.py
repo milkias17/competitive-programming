f5d84aa9df5715af823df03e12d974136eed4122
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-05-11 20:01:21 +0300

    Time: 1358 ms (35.40%) | Memory: 19.7 MB (42.31%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.py
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -1,21 +1,24 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        memo = [[[-1 for _ in range(n + 1)] for _ in range(m + 1)] for _ in range(len(strs) + 1)]
+        memo = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
         for i in range(m + 1):
             for j in range(n + 1):
-                memo[-1][i][j] = 0
+                memo[i][j] = 0
         
         for i in range(len(strs) - 1, -1, -1):
             cur = strs[i]
             zero_count = cur.count("0")
             one_count = cur.count("1")
 
+            new_memo = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
             for zeroes in range(m + 1):
                 for ones in range(n + 1):
                     include = 0
                     if zero_count <= zeroes and one_count <= ones:
-                        include = 1 + memo[i + 1][zeroes - zero_count][ones - one_count]
-                    skip = memo[i + 1][zeroes][ones]
-                    memo[i][zeroes][ones] = max(include, skip)
+                        include = 1 + memo[zeroes - zero_count][ones - one_count]
+                    skip = memo[zeroes][ones]
+                    new_memo[zeroes][ones] = max(include, skip)
+            
+            memo = new_memo
         
-        return memo[0][m][n]
+        return memo[m][n]
