70e79bd667326cbf8c33ac027c658112defba481
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-05-11 17:09:38 +0300

    Time: 365 ms (6.29%) | Memory: 19.4 MB (74.12%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,19 +1,24 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
-        memo = [[0 for _ in range(2 * (total + 1))] for _ in range(len(nums) + 1)]
-        for rep in range(2 * (total + 1)):
-            memo[-1][rep] = 1 if rep == target + total else 0
+        # memo = [[0 for _ in range(2 * (total + 1))] for _ in range(len(nums) + 1)]
+        # for rep in range(2 * (total + 1)):
+        #     memo[-1][rep] = 1 if rep == target + total else 0
+        
+        memo = [1 if rep == target + total else 0 for rep in range(2 * (total + 1))]
         
         for i in range(len(nums) - 1, -1, -1):
+            tmp = [0 for _ in range(2 * (total + 1))]
             for cur_sum in range(2 * (total + 1)):
                 count = 0
                 if cur_sum - nums[i] >= 0:
-                    count += memo[i + 1][cur_sum - nums[i]]
+                    count += memo[cur_sum - nums[i]]
                 if cur_sum + nums[i] < 2 * (total + 1):
-                    count += memo[i + 1][cur_sum + nums[i]]
+                    count += memo[cur_sum + nums[i]]
 
-                memo[i][cur_sum] = count
+                tmp[cur_sum] = count
+            
+            memo = tmp
         
-        return memo[0][total]
+        return memo[total]
             
