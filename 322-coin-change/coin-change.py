c3c6f8c0c9f6364c78960ebe39d59e5ef3b4373c
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-06-22 14:22:35 +0300

    Time: 879 ms (21.76%) | Memory: 20.7 MB (29.37%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,12 +1,40 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0
+        dp = [[-1 for a in range(amount + 1)] for _ in range(len(coins) + 1)]
+        for i in range(len(coins) + 1):
+            dp[i][0] = 0
+        
+        for a in range(1, amount + 1):
+            dp[-1][a] = -1
+        
+        current = [-1 for _ in range(amount + 1)]
+        nxt = [-1 for _ in range(amount + 1)]
+        current[0] = 0
+        nxt[0] = 0
 
-        for i in range(amount):
-            for coin in coins:
-                if i + coin < len(dp):
-                    dp[i + coin] = min(dp[i + coin], dp[i] + 1)
         
-        return dp[amount] if dp[amount] != float("inf") else -1
+        for i in range(len(coins) - 1, -1, -1):
+            for a in range(1, amount + 1):
+                opt1 = -1
+                if a - coins[i] >= 0:
+                    opt1 = current[a - coins[i]]
+                opt2 = nxt[a]
+
+                res = None
+                if opt1 == -1:
+                    res = opt2
+                elif opt2 == -1:
+                    res = 1 + opt1
+                else:
+                    res = min(1 + opt1, opt2)
+                
+                current[a] = res
+            
+            nxt = current
+            current = [-1 for _ in range(amount + 1)]
+            current[0] = 0
         
+        return nxt[amount]
+
+
+            
