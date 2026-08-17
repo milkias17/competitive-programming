10240c127290f7455a5e152a16c89f1d4ba02f46
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-08-17 11:11:04 +0300

    Time: 212 ms (86.54%) | Memory: 19.4 MB (97.86%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,14 +1,9 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        dp = [0 if j != len(t) else 1 for j in range(len(t) + 1)]
+        dp = [0] * len(t) + [1]
         for i in range(len(s) - 1, -1, -1):
-            nxt = [0] * (len(t) + 1)
-            nxt[len(t)] = 1
-            for j in range(len(t) - 1, -1, -1):
+            for j in range(len(t)):
                 if s[i] == t[j]:
-                    nxt[j] = dp[j + 1] + dp[j]
-                else:
-                    nxt[j] = dp[j]
-            dp = nxt
+                    dp[j] += dp[j + 1]
         
         return dp[0]
