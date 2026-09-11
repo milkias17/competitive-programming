673198fb298d7cf3018fa805adc5bdb8d58b048a
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-11 23:59:32 +0300

    Time: 2617 ms (5.00%) | Memory: 498.6 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,40 +1,27 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[-1 for a in range(amount + 1)] for _ in range(len(coins) + 1)]
-        for i in range(len(coins) + 1):
-            dp[i][0] = 0
-        
-        for a in range(1, amount + 1):
-            dp[-1][a] = -1
-        
-        current = [-1 for _ in range(amount + 1)]
-        nxt = [-1 for _ in range(amount + 1)]
-        current[0] = 0
-        nxt[0] = 0
+        memo = {}
 
-        
-        for i in range(len(coins) - 1, -1, -1):
-            for a in range(1, amount + 1):
-                opt1 = -1
-                if a - coins[i] >= 0:
-                    opt1 = current[a - coins[i]]
-                opt2 = nxt[a]
+        def dp(i, remaining):
+            if remaining == 0:
+                return 0
 
-                res = None
-                if opt1 == -1:
-                    res = opt2
-                elif opt2 == -1:
-                    res = 1 + opt1
-                else:
-                    res = min(1 + opt1, opt2)
-                
-                current[a] = res
+            if i >= len(coins) or remaining < 0:
+                return float("inf")
+
+            if (i, remaining) in memo:
+                return memo[(i, remaining)]
             
-            nxt = current
-            current = [-1 for _ in range(amount + 1)]
-            current[0] = 0
+            res =  min(
+                1 + dp(i, remaining - coins[i]),
+                1 + dp(i + 1, remaining - coins[i]),
+                dp(i + 1, remaining)
+            )
+            memo[(i, remaining)] = res
+            return res
         
-        return nxt[amount]
+        res = dp(0, amount)
+        if res == float("inf"):
+            return -1
 
-
-            
+        return res
