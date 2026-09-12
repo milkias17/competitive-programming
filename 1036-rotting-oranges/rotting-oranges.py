c1577785c93d010c06be274ce8c58be06e584b69
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-12 11:09:29 +0300

    Time: 5 ms (22.81%) | Memory: 19.4 MB (13.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,85 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        num_fresh = 0
-        queue = deque()
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    queue.append((i, j, 0))
-                elif grid[i][j] == 1:
-                    num_fresh += 1
-
-        neighbors = [[-1, 0], [1, 0], [0, 1], [0, -1]]
         visited = set()
-        count = 0
+        queue = deque()
+        rows, cols = len(grid), len(grid[0])
+        fresh_count = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                    visited.add((r, c))
+                if grid[r][c] == 1:
+                    fresh_count += 1
+        
+        def check_valid(r, c):
+            return min(r, c) >= 0 and r < rows and c < cols and (r, c) not in visited and grid[r][c] == 1
+        
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         minutes = 0
-        while queue:
-            row, col, height = queue.popleft()
-            visited.add((row, col))
-            minutes = max(minutes, height)
+        while queue and fresh_count > 0:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                for dx, dy in directions:
+                    nr, nc = r + dx, c + dy
 
-            for dx, dy in neighbors:
-                n_row, n_col = row + dx, col + dy
-                if (
-                    min(n_row, n_col) >= 0
-                    and n_row < rows
-                    and n_col < cols
-                    and (n_row, n_col) not in visited
-                    and grid[row][col] == 1
-                ):
-                    queue.append((row, col, height + 1))
-
-            if grid[row][col] == 1:
-                count += 1
-
-        if count != num_fresh:
-            print(num_fresh, count)
+                    if check_valid(nr, nc):
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
+                        fresh_count -= 1
+            
+            minutes += 1
+        
+        if fresh_count > 0:
             return -1
         
         return minutes
-class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        num_fresh = 0
-        queue = deque()
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    queue.append((i, j, 0))
-                elif grid[i][j] == 1:
-                    num_fresh += 1
-
-        neighbors = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        visited = set()
-        count = 0
-        minutes = 0
-        while queue:
-            row, col, height = queue.popleft()
-            # print(f"Checking {row}, {col}, {height}")
-            visited.add((row, col))
-            minutes = max(minutes, height)
-
-            for dx, dy in neighbors:
-                n_row, n_col = row + dx, col + dy
-                if (
-                    min(n_row, n_col) >= 0
-                    and n_row < rows
-                    and n_col < cols
-                    and (n_row, n_col) not in visited
-                    and grid[n_row][n_col] == 1
-                ):
-                    queue.append((n_row, n_col, height + 1))
-                    visited.add((n_row, n_col))
-
-            if grid[row][col] == 1:
-                count += 1
-
-        # print(f"Minutes: {minutes}")
-        if count != num_fresh:
-            # print(f"Wrong count: {count} != {num_fresh}")
-            return -1
-
-        return minutes
