7e7a0fc3586837e1191ddbca2d80022685e2422d
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-12 12:14:05 +0300

    Time: 734 ms (5.00%) | Memory: 19.6 MB (73.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,33 +1,27 @@
 class Solution:
-    def check_valid(self, s_counter, t_counter):
-        for k, v in t_counter.items():
-            if k not in s_counter or s_counter[k] < v:
-                return False
-        
-        return True
-
     def minWindow(self, s: str, t: str) -> str:
-        org_counter = Counter(t)
-        counter = Counter()
-        left = 0
-        min_subs = ""
-        min_length = float("inf")
-        for right in range(len(s)):
-            if s[right] in org_counter:
-                counter[s[right]] += 1
-                if counter[s[right]] == 0:
-                    del counter[s[right]]
-            
-            while self.check_valid(counter, org_counter):
-                # print(f"In for: {s[left:right+1]}, counter: {counter}")
-                if right - left + 1 < min_length:
-                    min_length = right - left + 1
-                    min_subs = s[left:right+1]
-                
-                if s[left] in counter:
-                    counter[s[left]] -= 1
-                left += 1
+        cur_counter = Counter()
+        t_counter = Counter(t)
 
-        return min_subs
+        def is_valid():
+            for char, count in t_counter.items():
+                if char not in cur_counter or cur_counter[char] < count:
+                    return False
                 
-                
+
+            return True
+        
+        left = 0
+        ans = None
+
+        for right in range(len(s)):
+            cur_counter[s[right]] += 1
+
+            while is_valid():
+                if ans is None or len(ans) > right - left + 1:
+                    ans = s[left:right + 1]
+                cur_counter[s[left]] -= 1
+                left += 1
+        
+        return ans if ans is not None else ""
+            
