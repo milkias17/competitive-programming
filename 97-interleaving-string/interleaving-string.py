e63ccf048937c99664be3ee6a8029b1d773c34cd
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-13 14:38:17 +0300

    Time: 42 ms (93.70%) | Memory: 20.6 MB (13.69%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,22 +1,27 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        dp = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
-        dp[len(s1)][len(s2)] = True
-
-        for k in range(len(s3) - 1, -1, -1):
-            new_dp = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
-
-            for i in range(len(s1), -1, -1):
-                for j in range(len(s2), -1, -1):
-                    if i < len(s1) and s1[i] == s3[k] and dp[i + 1][j]:
-                        new_dp[i][j] = True
-                    elif j < len(s2) and s2[j] == s3[k] and dp[i][j + 1]:
-                        new_dp[i][j] = True
-            
-            dp = new_dp
+        if len(s1) + len(s2) != len(s3):
+            return False
 
 
-        return dp[0][0]
-            
+        memo = {}
+        def dp(i, j, k):
+            if i >= len(s1) and j >= len(s2):
+                return True
 
+            if (i, j, k) in memo:
+                return memo[(i, j, k)]
                 
+            if i < len(s1) and s1[i] == s3[k] and dp(i + 1, j, k + 1):
+                memo[(i, j, k)] = True
+                return True
+            
+            if j < len(s2) and s2[j] == s3[k] and dp(i, j + 1, k + 1):
+                memo[(i, j, k)] = True
+                return True
+            
+            memo[(i, j, k)] = False
+            return False
+        
+        return dp(0, 0, 0)
+
