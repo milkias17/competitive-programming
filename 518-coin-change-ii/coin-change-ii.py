80ab05f9bac918d67ef257b9e6f3320fda699c42
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-13 15:30:55 +0300

    Time: 1640 ms (5.10%) | Memory: 518.5 MB (5.00%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,19 +1,14 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        current = [0] * (amount + 1)
-        nxt = [0] * (amount + 1)
-        current[0] = 1
-        nxt[0] = 1
         
-        for i in range(len(coins) - 1, -1, -1):
-            for a in range(1, amount + 1):
-                opt1 = current[a - coins[i]] if a - coins[i] >= 0 else 0
-                opt2 = nxt[a]
-                current[a] = opt1 + opt2
+        @lru_cache(maxsize=None)
+        def dp(i, target):
+            if target < 0 or i >= len(coins):
+                return 0
             
-            nxt = current
-            current = [0] * (amount + 1)
-            current[0] = 1
-
-
-        return nxt[amount]
+            if target == 0:
+                return 1
+            
+            return dp(i, target - coins[i]) + dp(i + 1, target)
+        
+        return dp(0, amount)
