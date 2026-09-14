f6538e3effd68200b56ff8b4c7c5990223fd253c
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-14 13:14:31 +0300

    Time: 3 ms (50.71%) | Memory: 19.3 MB (32.18%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,16 +1,12 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        answer = prices.copy()
         stack = []
-        for i in range(len(answer) - 1, -1, -1):
-            while len(stack) > 0 and stack[-1] > prices[i]:
-                stack.pop()
+        ans = prices.copy()
+
+        for i, price in enumerate(prices):
+            while stack and price <= prices[stack[-1]]:
+                ans[stack.pop()] -= price
             
-            if len(stack) == 0:
-                stack.append(prices[i])
-                continue
-            
-            answer[i] -= stack[-1]
-            stack.append(prices[i])
+            stack.append(i)
         
-        return answer
+        return ans
