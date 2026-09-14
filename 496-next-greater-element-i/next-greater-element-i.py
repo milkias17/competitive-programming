9dcef50abb9f54c5c233115d0814a5d59b461824
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-09-14 12:42:34 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.10%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,29 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num_idx_map = {}
-        greater_prefix = [-1] * len(nums2)
-        queue = deque()
+        next_greater = {}
 
-        for i in range(len(nums2) - 1, -1, -1):
-            num_idx_map[nums2[i]] = i
-            while queue and nums2[i] > nums2[queue[-1]]:
-                queue.pop()
-
-            if queue:
-                greater_prefix[i] = queue[-1]
-
-            queue.append(i)
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            
+            stack.append(num)
         
-        res = []
-        for num in nums1:
-            tmp = greater_prefix[num_idx_map[num]]
-            if tmp != -1:
-                res.append(nums2[tmp])
-            else:
-                res.append(tmp)
-        
-        return res
-
-        
-
-
+        return [next_greater[num] if num in next_greater else -1 for num in nums1]
